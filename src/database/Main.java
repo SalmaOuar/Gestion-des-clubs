dd34d99f583520d52cbdd8615a2785c50ea34342
SalmaOuar <ouarsalma34@gmail.com>
2025-03-10 00:12:25 +0000

    Add files via upload

---
+++ src/database/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+import java.sql.Connection;
+/**
+ *
+ * @author Yoga
+ */
+public class Main {
+    public static void main(String[] args) {
+        Connection conn = Connexion.getInstance().getCn();
+        if (conn != null) {
+            System.out.println("Connexion établie avec succès !");
+        } else {
+            System.out.println("Échec de la connexion !");
+        }
+    }
+}
