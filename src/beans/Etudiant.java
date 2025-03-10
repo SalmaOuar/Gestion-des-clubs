56efc0998b5919e859464715e293f036d058ea70
SalmaOuar <ouarsalma34@gmail.com>
2025-03-10 00:34:04 +0000

    Etudiant.java

--- src/beans/Etudiant.java
+++ src/beans/Etudiant.java
@@ -11,4 +11,47 @@ package beans;
  */
 public class Etudiant {
     
+    private int id;
+    private String nom;
+    private String prenom;
+    private String email;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Etudiant(int id, String nom, String prenom, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
 }
