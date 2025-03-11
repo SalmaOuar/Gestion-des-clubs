c52f6d2890fb388705dd8377f8febfe5abd5bbbb
SalmaOuar <ouarsalma34@gmail.com>
2025-03-11 16:41:41 +0000

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -29,12 +29,18 @@ public class Test {
         Club club1 = new Club(1, "Club Informatique", "Développement et programmation", new Date());
         Club club2 = new Club(2, "Club Robotique", "Innovation", new Date());
 
-        System.out.println("Ajout des clubs...");
         clubService.create(club1);
         clubService.create(club2);
+        Club club = clubService.findById(1);
+        if (club != null) {
+            System.out.println("Club trouvé : " + club.getNom());
+        }
+        club.setDescription("Nouvelles technologies et IA");
+        clubService.update(club);
+        System.out.println("Description du club mise à jour : " + clubService.findById(1).getDescription());
 
-        System.out.println("\nListe des clubs :");
         List<Club> clubs = clubService.findAll();
+        System.out.println("Liste des clubs :");
         for (Club c : clubs) {
             System.out.println("ID: " + c.getId() + " - Nom: " + c.getNom() + " - Description: " + c.getDescription());
         }
@@ -42,12 +48,18 @@ public class Test {
         Etudiant etudiant1 = new Etudiant(1, "Salma", "Ouarrar", "SalmaOuarrar@gmail.com");
         Etudiant etudiant2 = new Etudiant(2, "Israa", "Mountassir", "Israa22@gmail.com");
 
-        System.out.println("\nAjout des étudiants...");
         etudiantService.create(etudiant1);
         etudiantService.create(etudiant2);
+        Etudiant etudiant = etudiantService.findById(1);
+        if (etudiant != null) {
+            System.out.println("Étudiant trouvé : " + etudiant.getNom() + " " + etudiant.getPrenom());
+        }
+        etudiant.setPrenom("Doha");
+        etudiantService.update(etudiant);
+        System.out.println("Prénom de l'étudiant mis à jour : " + etudiantService.findById(1).getPrenom());
 
-        System.out.println("\nListe des étudiants :");
         List<Etudiant> etudiants = etudiantService.findAll();
+        System.out.println("Liste des étudiants :");
         for (Etudiant e : etudiants) {
             System.out.println("ID: " + e.getId() + " - Nom: " + e.getNom() + " - Prénom: " + e.getPrenom() + " - Email: " + e.getEmail());
         }
@@ -55,32 +67,22 @@ public class Test {
         Adhesion adhesion1 = new Adhesion(club1, etudiant1, new Date());
         Adhesion adhesion2 = new Adhesion(club2, etudiant2, new Date());
 
-        System.out.println("\nAjout des adhésions...");
         adhesionService.create(adhesion1);
         adhesionService.create(adhesion2);
 
-        System.out.println("\nListe des adhésions :");
         List<Adhesion> adhesions = adhesionService.findAll();
+        System.out.println("Liste des adhésions :");
         for (Adhesion a : adhesions) {
             System.out.println("Club: " + a.getClub().getNom() + " - Étudiant: " + a.getEtudiant().getNom() + " " + a.getEtudiant().getPrenom());
         }
 
-        System.out.println("\nMise à jour du Club Informatique...");
-        club1.setDescription("Nouvelles technologies et IA");
-        clubService.update(club1);
+        Adhesion adhesionSupprime = adhesionService.findAll().get(1);
+        adhesionService.delete(adhesionSupprime);
+        System.out.println("\nAdhésion supprimée : Club " + adhesionSupprime.getClub().getNom()
+                + " - Étudiant " + adhesionSupprime.getEtudiant().getNom() + " " + adhesionSupprime.getEtudiant().getPrenom());
 
-        System.out.println("\nListe des clubs après mise à jour :");
-        clubs = clubService.findAll();
-        for (Club c : clubs) {
-            System.out.println("ID: " + c.getId() + " - Nom: " + c.getNom() + " - Description: " + c.getDescription());
-        }
-
-        System.out.println("\nSuppression d'une adhésion...");
-        adhesionService.delete(adhesion1);
-
-        System.out.println("\nListe des adhésions après suppression :");
-        adhesions = adhesionService.findAll();
-        for (Adhesion a : adhesions) {
+        System.out.println("\nListe des adhésions restantes :");
+        for (Adhesion a : adhesionService.findAll()) {
             System.out.println("Club: " + a.getClub().getNom() + " - Étudiant: " + a.getEtudiant().getNom() + " " + a.getEtudiant().getPrenom());
         }
 
